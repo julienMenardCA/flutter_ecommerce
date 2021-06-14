cd172300fa094592453f28c8d715b028707a9983
Julien Menard <julien.menard@campus.academy>
2021-06-14 16:29:48 +0200

    Added "functionnal" homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const HomeScreen(),
     );
   }
 }
