94f949f0640ef888d6e53d9c2887b58a7b3fc91b
Julien Menard <julien.menard@campus.academy>
2021-06-14 16:50:11 +0200

    Started implementation of products BLoC

---
+++ lib/repositories/products_impl_repository.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_ecommerce/repositories/abstracts/products_repository.dart';
+
+class ProductsImplRepository extends ProductsRepository {
+
+}
---
+++ lib/services/products/products_bloc.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_ecommerce/repositories/abstracts/products_repository.dart';
+
+part 'products_event.dart';
+part 'products_state.dart';
+
+class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
+  final ProductsRepository _productsRepository;
+
+  ProductsBloc(this._productsRepository) : super(ProductsInitial());
+
+  @override
+  Stream<ProductsState> mapEventToState(
+    ProductsEvent event,
+  ) async* {
+    // TODO: implement mapEventToState
+  }
+}
