b3274570f9c5aebc821e0dc859e1741f48f13509
Julien Menard <julien.menard@campus.academy>
2021-06-15 16:37:57 +0200

    Ajout liste produits - Commencement

--- lib/services/products/products_bloc.dart
+++ lib/services/products/products_bloc.dart
@@ -5,6 +5,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_ecommerce/repositories/abstracts/products_repository.dart';
 
 part 'products_event.dart';
+
 part 'products_state.dart';
 
 class ProductsBloc extends Bloc<ProductsEvent, ProductsState> {
--- lib/services/products/products_event.dart
+++ lib/services/products/products_event.dart
@@ -2,4 +2,9 @@ part of 'products_bloc.dart';
 
 abstract class ProductsEvent extends Equatable {
   const ProductsEvent();
+
+  @override
+  List<Object> get props => [];
 }
+
+class ProductInitialEvent extends ProductsEvent {}
