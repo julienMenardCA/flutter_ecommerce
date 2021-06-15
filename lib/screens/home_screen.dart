5e20bd70e560b9d2f35e324567d2b2c5058c78e2
gregoire-tech <59013478+gregoire-tech@users.noreply.github.com>
2021-06-15 11:27:07 +0200

    test appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,12 +1,23 @@
+
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class Home_screen extends StatelessWidget {
+  const Home_screen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: null,
+
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Amalibaba"),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed:() => null,
+            ),
+          ]
+        ),
+      body: Center(child: null,)
     );
   }
 }
