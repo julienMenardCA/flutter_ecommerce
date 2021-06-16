0a6e24c25df74a1494ab3491a7be7aae2ac88d50
Clément Raulin <clem.moka49@gmail.com>
2021-06-16 17:02:40 +0200

    Update product_screen.dart

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -18,66 +18,74 @@ class _ProductScreen extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.white54,
-        ),
-        actions: [
-          IconButton(
-            onPressed: () => null,
-            icon: const Icon(Icons.shopping_cart),
-          )
-        ],
-      ),
-      body: SlidingUpPanel(
-        maxHeight: 500.0,
-        minHeight: 200.0,
-        borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(50.0),
-            topRight: Radius.circular(50.0)),
-        panel: Center(
-          child: Column(
-            children: [
-            Container(
-            width: 150.0,
-            height: 5.0,
-            color: Colors.black,
-            margin: const EdgeInsets.only(top: 15.0),
+        appBar: AppBar(
+          iconTheme: const IconThemeData(
+            color: Colors.white54,
           ),
-          Container(
-            margin: const EdgeInsets.only(top: 10.0),
-            width: 350.0,
-            child: Row(
-              children: [
-              Text(
-              _productImplRepository.getProductById(widget.index).title,
-              textAlign: TextAlign.left,
-              style: const TextStyle(fontSize: 24.0),
-            ),
-            ],
-          ),
+          actions: [
+            IconButton(
+              onPressed: () => null,
+              icon: const Icon(Icons.shopping_cart),
+            )
+          ],
         ),
-        Row(children: [
-          Container(
-            margin: const EdgeInsets.only(top: 50.0),
-            child: Text(
-              _productImplRepository.getProductById(widget.index).price.toString() + '€',
-              style: const TextStyle(fontSize: 18.0),
+        body: SlidingUpPanel(
+            maxHeight: 500.0,
+            minHeight: 200.0,
+            padding: const EdgeInsets.all(15.0),
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(50.0),
+                topRight: Radius.circular(50.0)),
+            panel: Center(
+              child: Column(
+                children: [
+                  Container(
+                    width: 150.0,
+                    height: 5.0,
+                    color: Colors.black,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      _productImplRepository.getProductById(widget.index).title,
+                      style: const TextStyle(fontSize: 28.0),
+                    ),
+                  ),
+                  Row(children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 20.0),
+                      child: Text(
+                        _productImplRepository
+                                .getProductById(widget.index)
+                                .price
+                                .toString() + '€',
+                        style: const TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                    Container(
+                      child: const Text(
+                        "Mis en favori par 1,2k visiteurs",
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () => null,
+                      icon: const Icon(Icons.favorite, color: Colors.red,),
+                    )
+                  ])
+                ],
+              ),
             ),
-          )
-        ])
-        ],
-      ),
-    ),
-    body: Center(
-    child: Padding(
-    padding: const EdgeInsets.only(bottom: 280.0, top: 20.0),
-    child: Container(
-    decoration: BoxDecoration(
-    image: DecorationImage(
-    image: NetworkImage(_productImplRepository.getProductById(widget.index).imageUrl))),
-    ),
-    ),
-    )));
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 280.0, top: 20.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: NetworkImage(_productImplRepository
+                              .getProductById(widget.index)
+                              .imageUrl))),
+                ),
+              ),
+            )));
   }
 }
