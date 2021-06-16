0d4cbd5af2b231c34c7884db48816caf452c90e7
Clément Raulin <clem.moka49@gmail.com>
2021-06-16 16:02:20 +0200

    Update Product Screen

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/config/data.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 
 class ProductScreen extends StatelessWidget {
@@ -10,6 +11,9 @@ class ProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white54,
+        ),
         actions: [
           IconButton(
             onPressed:() => null,
@@ -17,6 +21,60 @@ class ProductScreen extends StatelessWidget {
           )
         ],
       ),
+      body: SlidingUpPanel(
+        maxHeight: 500.0,
+        minHeight: 200.0,
+        panel: Center(
+          child: Column(
+            children: [
+              Container(
+                width: 150.0,
+                height: 5.0,
+                color: Colors.black,
+                margin: EdgeInsets.only(top: 15.0),
+              ),
+
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                width: 350.0,
+                child: Row(
+                  children: [
+                    Text(
+                      "${data[0]['title']}",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 24.0),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 50.0),
+                    child: Text(
+                        "${data[0]['price']} €",
+                        style: TextStyle(fontSize: 18.0),
+                    ),
+                  )
+                ]
+              )
+            ],
+          ),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 280.0, top: 20.0),
+            child: Container(
+              decoration:
+              BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(data[0]['image_url'] as String)
+                  )
+              ),
+            ),
+          ),
+        )
+      )
     );
   }
 }
